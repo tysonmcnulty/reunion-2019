aad624331fbe1ff4c59381034a6a9d02653c9c08
Tyson McNulty <tmcnulty@pivotal.io>
2019-05-10 22:32:46 -0400

    Add request logging

--- app/src/server.js
+++ app/src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const registrationApi = require("./registrationApi");
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.use("/api/registration", registrationApi);
 
